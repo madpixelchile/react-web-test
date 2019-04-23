3975b82a21535a11cf34f193b14d9a020e055424
cardumen <cardumen@cardumembpjuash.vtr.cl>
2019-04-23 14:33:50 -0400

    React router error 404 added - new properties of Route, Redirect, Exact path WTF

--- src/components/Page/PageContainer/PageContainer.js
+++ src/components/Page/PageContainer/PageContainer.js
@@ -1,21 +1,28 @@
 
 
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, Redirect} from 'react-router-dom'
 
 import { 
     PageHomeComponent, 
     PagePacksComponent,
-
+    PageError404Component,
+    
 } from '../';
 
 export class Container extends Component {
   render() {
+
+    const badUrlList = !Route.path;
+
     return (
       <div>
         <Switch>
-            <Route path='/home' component={PageHomeComponent}/>
+            <Route exact path='/' component={PageHomeComponent} />
+            <Route path="/home" component={PageHomeComponent}/>
             <Route path='/personas/packs' component={PagePacksComponent}/>
+            <Route path='/404' component={PageError404Component}/>
+            <Redirect from={badUrlList} to="/404" />
         </Switch>
       </div>
     );
